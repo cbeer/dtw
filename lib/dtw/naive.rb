cb51b15e8c1f3ee4804d053871cef2554b471ceb
Chris Beer <cabeer@stanford.edu>
2015-05-14 07:35:58 -0700

    Refactor warping matrix calculations

--- lib/dtw/naive.rb
+++ lib/dtw/naive.rb
@@ -53,48 +53,42 @@ module Dtw
     end
 
     def warping_matrix(g, i, j, z = 0)
-      return g.matrix[i, j] if g.matrix[i, j]
-      d = distance(i, j)
-
-      if window && !window.include?(i, j)
-        return Float::INFINITY
-      end
-
-      if i == 0 && j == 0
+      if g.matrix[i, j] # previously calculated
+        g.matrix[i, j]
+      elsif i < 0 || j < 0 || (window && !window.include?(i, j)) # outside window
+        Float::INFINITY
+      elsif i == 0 && j == 0 # base case
+        d = distance(i, j)
         g.path[i, j] = [nil, nil]
         g.matrix[i, j] = d
-        return d
+      else # recursion
+        # cost of the new cell is the minimum cost cell reachable using the pattern
+        # plus the distance of this cell
+        match = pattern.map do |(di, dj, weight)|
+          weight ||= 1
+
+          dii = di
+          djj = dj
+
+          cost = warping_matrix(g, i - di, j - dj, z + 1)
+
+          while dii > 1 && cost < Float::INFINITY
+            dii -= 1
+            cost += warping_matrix(g, i - dii, j - dj, z + 1)
+          end
+
+          while djj > 1 && cost < Float::INFINITY
+            djj -= 1
+            cost += warping_matrix(g, i - di, j - djj, z + 1)
+          end
+
+          [i - di, j - dj, weight * cost]
+        end.min_by { |(_, _, cost)| cost }
+
+        g.path[i, j] = match[0..1]
+
+        g.matrix[i, j] = match[2] + distance(i, j)
       end
-
-      match = pattern.map do |(di, dj, weight)|
-        weight ||= 1
-        next if ((i - di) < 0) || ((j - dj) < 0)
-
-        dii = di
-        djj = dj
-
-        cost = warping_matrix(g, i - di, j - dj, z + 1)
-
-        while dii > 1
-          dii -= 1
-          next if (i - dii) < 0
-          cost += warping_matrix(g, i - dii, j - dj, z + 1)
-        end
-
-        while djj > 1
-          djj -= 1
-          next if (j - djj) < 0
-          cost += warping_matrix(g, i - di, j - djj, z + 1)
-        end
-
-        [i - di, j - dj, weight * cost]
-      end.compact.min_by { |(_, _, cost)| cost }
-
-      match ||= [0, 0, warping_matrix(g, 0, 0), z + 1]
-
-      g.path[i, j] = match[0..1]
-
-      g.matrix[i, j] = match[2] + d
     end
 
     def distance(i, j)
